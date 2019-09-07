2ad597d85b41b2e8769c82b5213516d48baba403
sharifahblessing <snaluswata18@gmail,com>
2019-09-07 16:12:34 +0300

    Added price as part of the Burger controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,6 +36,9 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if(oldCount <=0 ){
+            return ;
+        }
         const updateCount = oldCount - 1;
         const updatedIngredients ={
             ...this.state.ingredients
@@ -55,7 +58,8 @@ class BurgerBuilder extends Component {
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler} />
+                    ingredientRemoved={this.removeIngredientHandler}
+                    price={this.state.totalPrice} />
             </Wrap>
         );
     }

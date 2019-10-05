3163f843e8db2ad7b71b2f168ce5ad136a3b0782
sharifahblessing <snaluswata18@gmail,com>
2019-10-05 17:30:04 +0300

    Added routes  and switch component in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React ,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import LayOut from './hoc/Layout/Layout'
 import BuildBurger from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 
-class App extends Component{
-  render(){
+import { Route, Switch } from 'react-router-dom'
+
+class App extends Component {
+  render() {
     return (
       <div>
         <LayOut>
-         <BuildBurger/>
-         <Checkout/>
+          <Switch>
+            <Route path='/Checkout' component={Checkout} />
+            <Route path='/' exact component={BuildBurger} />
+          </Switch>
         </LayOut>
       </div>
     );
   }
-  
+
 }
 
 export default App;
